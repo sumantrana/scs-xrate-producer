94f0d9bfa59b7f5477abe6616f133462b2bda06d
Sumant Singh Rana <rsumantsingh@vmware.com>
2023-02-15 16:00:58 +1100

    Added slf4j annotation

--- src/main/java/com/sumant/learning/messaging/xrateproducer/ExchangeRateProducer.java
+++ src/main/java/com/sumant/learning/messaging/xrateproducer/ExchangeRateProducer.java
@@ -1,5 +1,6 @@
 package com.sumant.learning.messaging.xrateproducer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @Configuration
+@Slf4j
 public class ExchangeRateProducer {
 
     private static HashMap<String, Double> exchangeRateDataMap = new HashMap<String, Double> (
@@ -19,17 +21,21 @@ public class ExchangeRateProducer {
 
     @Bean
     public Supplier<List<ExchangeRateRecord>> rateProducer(){
-        return () -> exchangeRateDataMap.keySet()
-                .stream()
-                .map( key -> {
-                    String[] components = key.split("-");
-                    String from = components[0];
-                    String to = components[1];
-                    Double rate = getRandomizedRate(key);
-                    ExchangeRateRecord exRecord = new ExchangeRateRecord(from, to , rate);
-                    System.out.println("Generated exchange rate record: " + exRecord.toString());
-                    return exRecord; })
-                .collect(Collectors.toList());
+        return () -> {
+            List<ExchangeRateRecord> recordList = exchangeRateDataMap.keySet()
+                    .stream()
+                    .map(key -> {
+                        String[] components = key.split("-");
+                        String from = components[0];
+                        String to = components[1];
+                        Double rate = getRandomizedRate(key);
+                        ExchangeRateRecord exRecord = new ExchangeRateRecord(from, to, rate);
+                        return exRecord;
+                    })
+                    .collect(Collectors.toList());
+                    log.info("Generated records: " + recordList);
+                    return recordList;
+        };
     }
 
     private Double getRandomizedRate(String currencyPair){
